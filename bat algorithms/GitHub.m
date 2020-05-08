025a1106b633796a8f1670ed913a7abda438960c
Haitao Yuan <65041551+haitaoyuan@users.noreply.github.com>
2020-05-08 17:38:09 -0400

    Update GitHub.m

--- bat algorithms/GitHub.m
+++ bat algorithms/GitHub.m
@@ -4,6 +4,17 @@ function [outputArg1,outputArg2] = GitHub(inputArg1,inputArg2)
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 
+% Files of the Matlab programs included in the book:       %
+% Xin-She Yang, Nature-Inspired Metaheuristic Algorithms,  %
+% Second Edition, Luniver Press, (2010).   www.luniver.com %
+% ======================================================== %
+
+% -------------------------------------------------------- %
+% Bat-inspired algorithm for continuous optimization (demo)%
+% Programmed by Xin-She Yang @Cambridge University 2010    %
+% -------------------------------------------------------- %
+% Usage: bat_algorithm([20 1000 0.5 0.5]);                 %
+
 
 
 end
