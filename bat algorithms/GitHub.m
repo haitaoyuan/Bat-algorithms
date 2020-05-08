5a7fe9ba404a285df76ee6e3b263e07ede9c97bd
Haitao Yuan <65041551+haitaoyuan@users.noreply.github.com>
2020-05-08 18:43:12 -0400

    Update GitHub.m

--- bat algorithms/GitHub.m
+++ bat algorithms/GitHub.m
@@ -8,18 +8,6 @@ clc;
 clear;
 clear global;
 
-% Display help
-%  help bat_algorithm.m
-
-% Default parameters
-if nargin<1
-    para=[20 1000 0.5 0.5];
-end
-
-n=para(1);      % Population size, typically 10 to 40
-N_gen=para(2);  % Number of generations
-A=para(3);      % Loudness  (constant or decreasing)
-r=para(4);      % Pulse rate (constant or decreasing)
 
 
 
